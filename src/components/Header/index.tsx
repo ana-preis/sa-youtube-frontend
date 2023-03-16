10f753a400abb610d7c8565a1a1a7fdb3e7a1f69
RAFAEL DO NASCIMENTO PEREIRA <rafael_n_pereira@estudante.sesisenai.org.br>
2023-03-15 21:40:54 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => {
+    return (
+        <div id="headerContainer">
+            <div>
+                <img src='./log2.svg' alt="logo"></img>
+            </div>
+            <div>asdf2</div>
+        </div>
+    )
+}
+
+export default Header;
