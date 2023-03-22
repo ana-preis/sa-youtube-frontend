3103261b52644387d65f2645c50c285fe43cba7d
ana-preis <anarps_@hotmail.com>
2023-03-22 19:22:46 -0300

    add row category to home

---
+++ src/components/SearchBar/index.tsx
@@ -0,0 +1,18 @@
+import './styles.css'
+
+const SearchBar = () => {
+    return (
+        <div className="flex-row search-bar">
+            <img src='./log2.svg' alt="logo" className='search-bar-logo'/>
+            <div className='flex-row search-container'>
+                <a href="" className="btn btn-3">Pesquisar</a>
+                <div className='flex-row'>
+                    <input className='search-input'/>
+                    <img src='./icon-search.svg' alt="icon-search" className='icon-search'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
