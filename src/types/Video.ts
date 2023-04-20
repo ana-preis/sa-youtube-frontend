470c3b408c47e87fd78dd96e2e9d89e5940bed2c
ANA RUTE PREIS E SILVA <ana_rp_silva@estudante.sesisenai.org.br>
2023-04-20 20:28:29 -0300

    fix types

--- src/types/Video.ts
+++ src/types/Video.ts
@@ -1,3 +1,5 @@
+import { Category } from "./Category";
+
 export interface VideoType {
   id: string;
   name: string;
@@ -10,3 +12,17 @@ export interface VideoType {
   dislikeCount?: number;
   rate?: number;
 }
+
+// export interface VideoReviewDetail {
+//   id: string,
+//   categoryList: Category[],
+//   title: string,
+//   embedHtml: string,
+//   description: string,
+//   tags: string[],
+//   publishedAt: string,
+//   channelName: string,
+//   likeCount: number,
+//   viewCount: number,
+//   userToWatch: Video[],
+// }
